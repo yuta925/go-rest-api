b43664e524dd7566d225503d89e8109a92a9328b
yuta925 <yutarou041@gmail.com>
2024-03-06 02:28:10 +0900

    Taskのrouter実装

--- router/router.go
+++ router/router.go
@@ -2,14 +2,26 @@ package router
 
 import (
 	"go-rest-api/controller"
+	"os"
 
+	echojwt "github.com/labstack/echo-jwt/v4"
 	"github.com/labstack/echo/v4"
 )
 
-func NewRouter(uc controller.IUserController) *echo.Echo {
+func NewRouter(uc controller.IUserController, tc controller.ITaskController) *echo.Echo {
 	e := echo.New()
 	e.POST("/signup", uc.SignUp)
 	e.POST("/login", uc.Login)
 	e.POST("/logout", uc.Logout)
+	t := e.Group("/tasks")
+	t.Use(echojwt.WithConfig(echojwt.Config{
+		SigningKey: []byte(os.Getenv("SECREt")),
+		TokenLookup: "cookie:token",
+	}))
+	t.GET("", tc.GetAllTasks)
+	t.GET("/:task_id", tc.GetTaskById)
+	t.POST("", tc.CreateTask)
+	t.PUT("/:task_id", tc.UpdateTask)
+	t.DELETE("/:task_id", tc.DeleteTask)
 	return e
 }
