a296140e3d6f3fa6c97f5bb4189a073fb351d38e
yuta925 <yutarou041@gmail.com>
2024-03-05 23:59:39 +0900

    usecaseとrepositoryのinterface実装

--- model/user.go
+++ model/user.go
@@ -4,14 +4,14 @@ import "time"
 
 type User struct {
 	ID uint `json:"id" gorm:"primary_key"`
-	Email string `json:"email" gorm:"unique`
+	Email string `json:"email" gorm:"unique"`
 	Password string `json:"password"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
 
 // 新しいユーザーの情報をクライアントにレスポンスで返すときのデータの型定義
-type UserReponse struct {
+type UserResponse struct {
 	ID uint `json:"id" gorm:"primary_key"`
-	Email string `json: "email" gorm:"unique"`
+	Email string `json:"email" gorm:"unique"`
 }
