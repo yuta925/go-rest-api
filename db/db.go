7a29e6951035c42cf4051a538db797a60fb53f19
yuta925 <yutarou041@gmail.com>
2024-03-05 17:45:24 +0900

    successfully migrated

--- db/db.go
+++ db/db.go
@@ -1,10 +1,9 @@
 package db
 
 import (
+	"fmt"
 	"log"
 	"os"
-
-	"fmt"
 
 	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
@@ -13,22 +12,19 @@ import (
 
 func NewDB() *gorm.DB {
 	if os.Getenv("GO_ENV") == "dev" {
-		err := godotenv.Load() 
+		err := godotenv.Load()
 		if err != nil {
 			log.Fatalln(err)
 		}
 	}
-
-	url := fmt.Sprintf("postgress://%s:%s:%s:%s:%s", os.Getenv("POSTGRES_USER"),
-		os.Getenv("POSTGRES_PW"),os.Getenv("POSTGRES_HOST"),
+	url := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PW"), os.Getenv("POSTGRES_HOST"),
 		os.Getenv("POSTGRES_PORT"), os.Getenv("POSTGRES_DB"))
-
 	db, err := gorm.Open(postgres.Open(url), &gorm.Config{})
-	
 	if err != nil {
 		log.Fatalln(err)
-	} 
-	fmt.Println("Conected")
+	}
+	fmt.Println("Connceted")
 	return db
 }
 
